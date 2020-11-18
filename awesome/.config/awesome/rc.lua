cbc0eb8ef347e67f2101df78089e0637b33f0b41
Treor <kostgromov@yahoo.com>
2020-11-18 09:35:47 +0000

    Fix(awesome): Tasklist widget Now I can bring programs back from oblivion

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -253,8 +253,8 @@ awful.screen.connect_for_each_screen(function(s)
             s.txtlayoutbox,
             s.mypromptbox,
         },
-        nil,
-        --s.mytasklist, -- Middle widget
+        --nil,
+        s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
