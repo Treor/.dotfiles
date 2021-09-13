99963c67a0104535c379394f06f69341021b4836
Treor <kostgromov@yahoo.com>
2021-09-13 19:28:42 +0300

    Feat(AwesomeWM): simplified choise of layouts from 7 down to 3

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -75,12 +75,12 @@ editor_cmd = terminal .. " -e " .. editor
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.fair,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
+    awful.layout.suit.floating,
+    awful.layout.suit.max,
+    --awful.layout.suit.spiral.dwindle,
+    --awful.layout.suit.fair,
+    --awful.layout.suit.max.fullscreen,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
