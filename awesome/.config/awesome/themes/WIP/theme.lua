fe97c74618a15f8b8fa00d390decde80fa3834ef
Treor <kostgromov@yahoo.com>
2021-09-13 17:53:01 +0300

    Feat(AwesomeWM): New theme

---
+++ awesome/.config/awesome/themes/WIP/theme.lua
@@ -0,0 +1,212 @@
+---------------------------------------------
+-- Awesome theme which follows xrdb config --
+--   by Yauhen Kirylau                    --
+---------------------------------------------
+
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+local xrdb = xresources.get_current_theme()
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+-- inherit default theme
+local theme = dofile(themes_path .. "default/theme.lua")
+-- load vector assets' generators for this theme
+
+theme.font          = "sans-serif Bold"..dpi(8)
+theme.iconfont      = "sans-serif Bold"..dpi(6)
+theme.boldfont      = "sans-serif Bold "..dpi(10)
+
+--theme.bg_normal     = xrdb.background .."00"
+theme.bg_normal     = xrdb.background
+theme.bg_focus      = theme.bg_normal
+theme.bg_urgent     = theme.bg_normal
+theme.bg_minimize   = theme.bg_normal
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = xrdb.foreground
+theme.fg_focus      = xrdb.color4
+theme.fg_urgent     = xrdb.color9
+theme.fg_minimize   = xrdb.color8
+
+theme.useless_gap   = dpi(3)
+theme.gap_single_client = true
+theme.border_width  = dpi(1)
+theme.border_normal = xrdb.color0
+theme.border_focus  = theme.bg_focus
+theme.border_marked = xrdb.color10
+
+theme.underline_color = xrdb.color14
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+theme.taglist_fg_empty = xrdb.color8
+theme.taglist_fg_occupied = xrdb.color15
+theme.taglist_fg_focus = xrdb.color4
+theme.taglist_fg_urgent = xrdb.color9
+
+theme.tooltip_fg = theme.fg_normal
+theme.tooltip_bg = theme.bg_normal
+
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(25)
+theme.menu_width  = dpi(100)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+
+theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+
+theme.wallpaper = themes_path.."default/background.png"
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+theme.layout_max = themes_path.."default/layouts/maxw.png"
+theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+theme.layout_tile = themes_path.."default/layouts/tilew.png"
+theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+
+theme.layout_txt_tile       = "[t]"
+theme.layout_txt_tileleft   = "[l]"
+theme.layout_txt_tilebottom = "[b]"
+theme.layout_txt_tiletop    = "[tt]"
+theme.layout_txt_fairv      = "[fv]"
+theme.layout_txt_fairh      = "[fh]"
+theme.layout_txt_spiral     = "[s]"
+theme.layout_txt_dwindle    = "[d]"
+theme.layout_txt_max        = "[m]"
+theme.layout_txt_fullscreen = "[F]"
+theme.layout_txt_magnifier  = "[M]"
+theme.layout_txt_floating   = "[*]"
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Recolor Layout icons:
+theme = theme_assets.recolor_layout(theme, theme.fg_normal)
+
+-- Recolor titlebar icons:
+--
+local function darker(color_value, darker_n)
+    local result = "#"
+    for s in color_value:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
+        local bg_numeric_value = tonumber("0x"..s) - darker_n
+        if bg_numeric_value < 0 then bg_numeric_value = 0 end
+        if bg_numeric_value > 255 then bg_numeric_value = 255 end
+        result = result .. string.format("%2.2x", bg_numeric_value)
+    end
+    return result
+end
+theme = theme_assets.recolor_titlebar(
+    theme, theme.fg_normal, "normal"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, darker(theme.fg_normal, -60), "normal", "hover"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, xrdb.color1, "normal", "press"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, theme.fg_focus, "focus"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, darker(theme.fg_focus, -60), "focus", "hover"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, xrdb.color1, "focus", "press"
+)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+ theme.icon_theme = "/usr/share/icons/Papirus"
+ --theme.icon_theme = "Papirus"
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(0)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
+-- Try to determine if we are running light or dark colorscheme:
+local bg_numberic_value = 0;
+for s in theme.bg_normal:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
+    bg_numberic_value = bg_numberic_value + tonumber("0x"..s);
+end
+local is_dark_bg = (bg_numberic_value < 383)
+
+-- Generate wallpaper:
+local wallpaper_bg = xrdb.color8
+local wallpaper_fg = xrdb.color7
+local wallpaper_alt_fg = xrdb.color12
+if not is_dark_bg then
+    wallpaper_bg, wallpaper_fg = wallpaper_fg, wallpaper_bg
+end
+theme.wallpaper = function(s)
+    return theme_assets.wallpaper(wallpaper_bg, wallpaper_fg, wallpaper_alt_fg, s)
+end
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
