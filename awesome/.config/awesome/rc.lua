3a55ee0caa03174df2b91447d546c2c1a318fba7
Treor <kostgromov@yahoo.com>
2021-09-13 22:56:06 +0300

    Feat(AwesomeWM): cleaned rc.lua a bit

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1,23 +1,7 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, "luarocks.loader")
--- Standard awesome library
-local gears = require("gears")
-local awful = require("awful")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
-local beautiful = require("beautiful")
-local xresources = require("beautiful.xresources")
-local dpi = xresources.apply_dpi
--- Notification library
-local naughty = require("naughty")
-local menubar = require("menubar")
---local task_list = require('configuration.widgets.task-list')
-
 -- ===================================================================
 local themes = {
     "xresources",        -- 1 --
+    "WIP",
 }
 -- Change this number to use a different theme
 local theme = themes[1]
@@ -25,14 +9,81 @@ local theme = themes[1]
 -- Statusbar themes. Multiple bars can be declared in each theme.
 local bar_themes = {
     "mybar",        -- 1 -- Taglist, client counter, date, time, layout
+    "WIP",
 }
 local bar_theme = bar_themes[1]
 -- ===================================================================
 
-require('configuration.client')
-_G.root.keys(require('configuration.keys.global'))
-require('module.autostart')
--- {{{ Error handling
+-- This is used later as the default terminal and editor to run.
+terminal = "kitty"
+editor = os.getenv("EDITOR") or "nano"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Initialization
+-- ===================================================================
+-- Theme handling library
+local beautiful = require("beautiful")
+local xrdb = beautiful.xresources.get_current_theme()
+-- Make dpi function global
+dpi = beautiful.xresources.apply_dpi
+-- Make xresources colors global
+x = {
+    --           xrdb variable
+    background = xrdb.background,
+    foreground = xrdb.foreground,
+    color0     = xrdb.color0,
+    color1     = xrdb.color1,
+    color2     = xrdb.color2,
+    color3     = xrdb.color3,
+    color4     = xrdb.color4,
+    color5     = xrdb.color5,
+    color6     = xrdb.color6,
+    color7     = xrdb.color7,
+    color8     = xrdb.color8,
+    color9     = xrdb.color9,
+    color10    = xrdb.color10,
+    color11    = xrdb.color11,
+    color12    = xrdb.color12,
+    color13    = xrdb.color13,
+    color14    = xrdb.color14,
+    color15    = xrdb.color15,
+}
+
+-- Load AwesomeWM libraries
+local gears = require("gears")
+local awful = require("awful")
+require("awful.autofocus")
+
+local function set_wallpaper(s)
+    -- Wallpaper
+    if beautiful.wallpaper then
+        local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        if type(wallpaper) == "function" then
+            wallpaper = wallpaper(s)
+        end
+        gears.wallpaper.maximized(wallpaper, s, true)
+    end
+end
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+--screen.connect_signal("property::geometry", set_wallpaper)
+
+awful.screen.connect_for_each_screen(function(s)
+    -- Wallpaper
+    --awful.spawn.with_shell("hsetroot -add '#FFC0CB' -add '#3D4C5F' -gradient 0")
+    awful.spawn.with_shell("hsetroot -add '#F48FB1' -add '#323F4E' -gradient 0")
+    --set_wallpaper(s)
+end)
+
+-- Default notification library
+local naughty = require("naughty")
+
+-- Load theme
+local theme_dir = os.getenv("HOME") .. "/.config/awesome/themes/" .. theme .. "/"
+beautiful.init(theme_dir .. "theme.lua")
+
+-- Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
@@ -56,22 +107,6 @@ do
         in_error = false
     end)
 end
--- }}}
-
--- Load theme
-local theme_dir = os.getenv("HOME") .. "/.config/awesome/themes/" .. theme .. "/"
-beautiful.init(theme_dir .. "theme.lua")
-
--- This is used later as the default terminal and editor to run.
-terminal = "kitty"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
@@ -94,40 +129,23 @@ awful.layout.layouts = {
 }
 -- }}}
 
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
-end
+-- >> Features
+require('configuration.client')
+_G.root.keys(require('configuration.keys.global'))
+require('module.autostart')
 
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
---screen.connect_signal("property::geometry", set_wallpaper)
-
-awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    --awful.spawn.with_shell("hsetroot -add '#FFC0CB' -add '#3D4C5F' -gradient 0")
-    awful.spawn.with_shell("hsetroot -add '#F48FB1' -add '#323F4E' -gradient 0")
-    --set_wallpaper(s)
-end)
--- }}}
-
--- >> Elements - Desktop components
--- Statusbar(s)
-require("elements.bar."..bar_theme)
-
--- {{{ Mouse bindings
+-- Mouse bindings
 root.buttons(gears.table.join(
     --awful.button({ }, 3, function () mymainmenu:toggle() end),
     --awful.button({ }, 4, awful.tag.viewnext),
     --awful.button({ }, 5, awful.tag.viewprev)
 ))
--- }}}
+
+-- >> Elements - Desktop components
+-- Statusbar(s)
+require("elements.bar."..bar_theme)
+
+
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
